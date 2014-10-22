4bb495765199959e2a3ca1b9947847952793dcfb
ezlotnik <elisez97@gmail.com>
2014-10-22 18:12:20 -0400

    EVERYTHING WORKS, JUST GOTTA MAKE IT PRETTY NOW

--- test.py
+++ test.py
@@ -18,28 +18,46 @@ def main(titles=None):
         text = request.form["textarea"]
         poster = request.form["author"]
         time = strftime("%a %d %b %Y %X")
-        
         dbhelper.connect()
         dbhelper.insert_post(poster, title, text, time)
         titles = dbhelper.get_posts()
         dbhelper.close()
         return render_template("home.html",titles=titles)
 
-@test.route("/<title>")
+@test.route("/<title>",methods=["GET","POST"])
 def blogpost(title=None,post=None,comments=None):
-    dbhelper.connect()
-    post = dbhelper.get_post(title)
-    comments = dbhelper.get_comments(title)
-    titles = dbhelper.get_posts()
-    author=""
-    for item in titles: 
-        if item[1] == title: 
-            author = item[0] 
-            break
-    dbhelper.close()
-    return render_template("blogpost.html",title=title,author=author,
+    if request.method=="GET":
+        dbhelper.connect()
+        post = dbhelper.get_post(title)
+        comments = dbhelper.get_comments(title)
+        titles = dbhelper.get_posts()
+        author=""
+        for item in titles: 
+            if item[1] == title: 
+                author = item[0] 
+                break
+        dbhelper.close()
+        return render_template("blogpost.html",title=title,author=author,
                             post=post,
                             comments=comments)
+    else:
+        button = request.form["submit"]
+        poster = request.form["author"]
+        text = request.form["textarea"]
+        time = strftime("%a %d %b %Y %X")
+        dbhelper.connect()
+        dbhelper.insert_comment(title, poster, text, time)
+        post = dbhelper.get_post(title)
+        comments = dbhelper.get_comments(title)
+        titles = dbhelper.get_posts()
+        author=""
+        for item in titles: 
+            if item[1] == title: 
+                author = item[0] 
+                break
+        dbhelper.close()
+        return render_template("blogpost.html",title=title,author=author,
+                               post=post,comments=comments)
     
 if __name__ == "__main__":
     test.debug = True
